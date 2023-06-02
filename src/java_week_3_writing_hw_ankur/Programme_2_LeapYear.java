bd5c8cba2aa5c16623f0f6ddb049313101f8ffc3
jeck16 <khushiraval90@gmail.com>
2023-06-02 13:00:54 +0100

    java-week-3-hw

---
+++ src/java_week_3_writing_hw_ankur/Programme_2_LeapYear.java
@@ -0,0 +1,24 @@
+package java_week_3_writing_hw_ankur;
+
+import java.util.Scanner;
+
+public class Programme_2_LeapYear {
+    public static void main(String[] args) {
+        // Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the year");
+        int year = scanner.nextInt();
+        Programme_2_LeapYear leapYear = new Programme_2_LeapYear();
+        // closing the scanner object
+        scanner.close();
+    }
+
+    // Checking is it leap year or not
+    public void isItLeapYear(int year) {
+        if(year % 4==0 && 100 != 0 || year % 400 == 0){
+            System.out.println("The year"  + year + " is leap year");
+            return;
+        }
+        System.out.println("The year" + year + " is leap year");
+    }
+}
